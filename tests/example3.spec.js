6d3c96816a2f6f3649ac379b591fe09791ae0dd9
Tuong Long <ttrhailong2804@gmail.com>
2025-10-19 20:54:57 +0700

    first commit

---
+++ tests/example3.spec.js
@@ -0,0 +1,56 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Header & Navigation', () => {
+
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://demowebshop.tricentis.com/');
+  });
+
+  test('Navigation main by logo ', async ({ page }) => {
+    
+    await page.getByRole('link', { name: 'Demo Web Shop' }).click();
+    await expect(page).toHaveURL('https://demowebshop.tricentis.com/');
+    await expect(page.getByRole('heading', { name: 'welcome to our store' })).toBeVisible();
+  });
+
+  test(' Register header ', async ({ page }) => {
+    
+    await page.getByRole('link', { name: 'register' }).click();
+    await expect(page).toHaveURL('https://demowebshop.tricentis.com/register');
+    await expect(page.getByRole('heading', { name: 'Register' })).toBeVisible();
+    await expect(page.getByText('Your Personal Details')).toBeVisible();
+
+  });
+
+  test('Navigation to Login', async ({ page }) => {
+
+   await page.getByRole('link', { name: /log in/i }).click(); 
+   await expect(page).toHaveURL('https://demowebshop.tricentis.com/login');
+   await expect(page.getByRole('heading', { name: /Welcome,/i})).toBeVisible();
+  
+  });
+
+  test('Navigation to Shopping Cart', async ({ page }) => {
+
+    await page.getByRole('link', { name: /Shopping cart/ }).nth(0).click();
+    await expect(page).toHaveURL('https://demowebshop.tricentis.com/cart');
+    await expect(page.getByRole('heading', { name: 'Shopping cart', exact: true })).toBeVisible();
+
+  });
+
+  test('Navigation to wishlist', async ({ page}) => {
+
+    await page.getByRole('link', {name: 'wishlist'}).nth(0).click();
+    await expect(page).toHaveURL('https://demowebshop.tricentis.com/wishlist');
+    await expect(page.getByRole('heading', {name: 'Wishlist', exact: true })).toBeVisible();
+
+  });
+  
+  test.afterEach(async ({ page }) => {
+
+    await page.goto('https://demowebshop.tricentis.com/');
+
+  });
+
+
+});
